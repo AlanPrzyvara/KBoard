3c974c7267a914143a0c6846beb379c028bd2ddd
YiipHuhu <alan.gabriel@nullbug.dev>
2025-05-20 18:19:07 -0300

    ajuste de cors

---
+++ src/main/kotlin/com/kboard/config/CorsConfig.kt
@@ -0,0 +1,25 @@
+package com.kboard.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("http://localhost:3000") // URL do seu frontend
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+
+        return CorsFilter(source)
+    }
+} 
