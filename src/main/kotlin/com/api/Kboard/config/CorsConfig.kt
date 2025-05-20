152ffd09c6cbbee821b19605fbcd08df7ed039ec
YiipHuhu <alan.gabriel@nullbug.dev>
2025-05-20 19:11:52 -0300

    api publica para o projeto 2

--- src/main/kotlin/com/api/Kboard/config/CorsConfig.kt
+++ src/main/kotlin/com/api/Kboard/config/CorsConfig.kt
@@ -1,6 +1,10 @@
 package com.api.kboard.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -8,11 +12,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/api/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("*")
             .allowedHeaders("*")
-            .exposedHeaders("Authorization")
+            .allowCredentials(true)
             .maxAge(3600)
+    }
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        
+        config.allowCredentials = true
+        config.addAllowedOriginPattern("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        config.maxAge = 3600L
+        
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
     }
 } 
